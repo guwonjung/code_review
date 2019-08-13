543d56f2c460e69ad5a9d5494a32b11b2bbfd801
Guwon Jeong <guwon.jung@samsung.com>
2019-08-13 16:00:29 +0900

    integer 외 다른 type도 지원하도록 수정 및 Google style guide 적용

--- project1/priority_queue.h
+++ project1/priority_queue.h
@@ -1,15 +1,53 @@
 #include <queue>
 
-template <typename T> 
-class templatePriorityQueue {
-	private:
-		std::priority_queue<T> storage;
-	public:
-		templatePriorityQueue();
-		~templatePriorityQueue();
-		bool empty() const;
-		const T& top() const;
-		int size();
-		void push(const T&);
-		void pop();	
+template <typename T>
+class TemplatePriorityQueue {
+ public:
+  explicit TemplatePriorityQueue();
+  ~TemplatePriorityQueue();
+
+  bool empty() const;
+  const T& top() const;
+  int size() const;
+  void push(const T&);
+  void pop();
+
+ private:
+  std::priority_queue<T> storage_;
 };
+
+template <typename T>
+TemplatePriorityQueue<T>::TemplatePriorityQueue() {
+}
+
+template <typename T>
+TemplatePriorityQueue<T>::~TemplatePriorityQueue() {
+  while (!storage_.empty()) {
+    storage_.pop();
+  }
+}
+
+template <typename T>
+bool TemplatePriorityQueue<T>::empty() const {
+  return storage_.empty();
+}
+
+template <typename T>
+int TemplatePriorityQueue<T>::size() const {
+  return storage_.size();
+}
+
+template <typename T>
+const T& TemplatePriorityQueue<T>::top() const {
+  return storage_.top();
+}
+
+template <typename T>
+void TemplatePriorityQueue<T>::pop() {
+  storage_.pop();
+}
+
+template <typename T>
+void TemplatePriorityQueue<T>::push(const T& val) {
+  storage_.push(val);
+}
